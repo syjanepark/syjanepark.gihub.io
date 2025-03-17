fa30851c3133eb1dd6c25448f662218d6904fbda
Jane Park <157393457+syjanepark@users.noreply.github.com>
2025-03-17 17:47:02 -0500

    with hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router'
 import SiteLayout from './components/SiteLayout'
 import Home from './components/Home'
 import MyWork from './components/MyWork'
 
 function App() {
-  return <BrowserRouter>
+  return <HashRouter>
     <Routes>
       <Route path='/' element={<SiteLayout/>}>
         <Route index element={<Home/>}></Route>
         <Route path='my-work' element={<MyWork/>}></Route>
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 }
 
 export default App
